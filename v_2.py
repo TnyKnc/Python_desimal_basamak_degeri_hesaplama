b48f46c70a72e076a07c86130ec6ef085ccbfab8
Tunay KINICI <tunay6734@mynet.com>
2017-08-03 20:33:42 +0300

    Update v_2.py

--- v_2.py
+++ v_2.py
@@ -10,41 +10,40 @@ def basamak_hesabi(sayi): #basamak hesaplarını yaptırmak için basamak_hesabi
     sayac=0 #basamak hesabını tutması için sayac değişkeni oluşturup 0 dan başlattım.
     sabit=int(sayi) #ondalıklı sayılarda virgülden öncesini hesaplatmak için sabit adında değişken oluşturup girilen sayının virgül solundaki kısmını aldım.
     
-    #sayi=sayi-int(sayi)+1
-    #print(bin(sayi))
-    
-    #print(sayi)
-    #print(str(sayi)[3])
-
-    yedek_sayi=sayi
-    i=0
-    son_sayi=""
-    stSayi=str(sayi)+".."
+    yedek_sayi=sayi #programın sonuna kadar sayının tamamını tutacağım yedek değişken oluşturdum.
+    i=0 #döngü için i değişkeni oluşturdum.
+    son_sayi="" #string olarak sayıları virgülden sonra basamak basamak alacağım değişkeni oluşturdum.
+    stSayi=str(sayi)+"." #string olarak tuttuğum sayıları toplu halde atacağım değişkeni oluşturdum.
+    #en sonuna . koyup \0 biçiminde döngüde sayılar sonlansa dahi hata vermemesini sağladım.
     if sayi>int(sayi): #sayının desimal olup olmadığını int türündeki karşılığını yaparak kontrol ettirdim.
         #eğer sayi int türündeki değerinden büyükse desimaldir. Alt satırlardaki işlemleri ona göre yaptırdım.
         
-
-        while stSayi[i]!=".":
-            i=i+1
-        i=i+1
-        while stSayi[i]!=".":
-            #print(str(stSayi)[i])
-            son_sayi+=str(stSayi)[i]
-            #print(son_sayi)
-            i=i+1
-        #print(son_sayi)
-    
-        son_sayi=int(son_sayi)
+        while stSayi[i]!=".": #string olarak tuttuğum sayı değişkeninin her hanesini . olana kadar kontrol ettirdim.
+            i=i+1 #i değişkenini her seferinde 1 arttırıp döngünün devamlılığını sağladım.
         
-    while int(sayi)>0.9: #sayı desimal olduğu sürece döngü çalışacak.
-        sayi/=10 #sayi değişkenini 10'a bölerek her seferinde 1 azaltıyorum.
+        i=i+1 #döngü bittiğinde i değişkenini tekrar 1 arttırıp nokta işaretinden sonraki basamağa geçmesini sağladım.
+        
+        while stSayi[i]!=".": #string olarak tuttuğum sayı değişkenini tekrar . olana kadar kontrol ettirdim.
+            #buradaki nokta en sonda bulunan nokta oluyor. İlk döngüde desimal değerin noktasını atlamıştık.
+            son_sayi+=str(stSayi)[i] #son_sayi adli string değişkene stSayi değişkenindeki rakamları ardı ardına atadım.
+            i=i+1 #i değişkenini tekrar döngüde 1 arttırarak sonraki basamağa geçişi sağladım.
+    
+        son_sayi=int(son_sayi) #string olan son_sayi değişkenini integer tipine çevirdim.
+        
+    while int(sayi)>0.9: #sayı .9dan büyük olduğu sürece döngü dönecek.
+        sayi/=10 #sayi değişkenini 10'a bölerek her seferinde 1 basamak azaltıyorum.
         sayac+=1 #sayac değişkenini her seferinde 1 arttırarak basamak hesabını hafızada tutuyorum.
-    if yedek_sayi>int(yedek_sayi):
-        return sayac + basamak_hesabi(son_sayi) #sayac değişkenini (basamak değerini) fonksiyondan geri yolluyorum.
+    
+    if yedek_sayi>int(yedek_sayi): #yedek sayının desimal olup olmadığını kontrol ediyorum.
+        #yedek_sayi değişkeninin int hali virgülden sonrasını almayacağı için daha küçük olacak.
+        return sayac + basamak_hesabi(son_sayi) #sayac değişkenini (basamak değerini) ve 
+    #son_sayi(virgülden sonraki sayı)yı toplatıp fonksiyondan geri döndürüyorum.
+    
     else:
-        return sayac
+        return sayac #yedek_sayi değişkenimiz yani girilen sayımız desimal değilse virgül
+    #sonrasını hesaplamayacağımız için sayac yani hesaplanan değeri geri döndürüyorum.
 
 
 
-print ("desimalsiz bölümün uzunluğu=",basamak_hesabi(1234.56789)) #basamak hesabını sayıyı yollayarak ekrana bastırıyorum.
+print ("desimalsiz bölümün uzunluğu=",basamak_hesabi(d)) #basamak hesabını sayıyı yollayarak ekrana bastırıyorum.
 print ("desimal ile bölümün uzunluğu=",basamak_hesabi(c))
